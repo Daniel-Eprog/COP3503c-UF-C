7a27b55154875b35a54c1c43699c508fcef5f739
Daniel-Eprog <39196999+Daniel-Eprog@users.noreply.github.com>
2022-09-29 20:04:09 -0400

    first commit lab 3

---
+++ cop3503labs/lab3_stackqueue/ABQ.h
@@ -0,0 +1,21 @@
+/*
+Esteban Medero COP3503c Lab3 - Array Based Queue
+October 8, 2022
+This is part one of lab3 building a dynamically allocated array based queue.
+a queue is a data structure that follows the First In First Out principle
+the next item is added behind the previous item hence the first item remains in 
+the front. Think people forming a line.
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+template <typename ABStype>
+class ABS
+{
+    private:
+
+    public:
+
+};
---
+++ cop3503labs/lab3_stackqueue/ABS.h
@@ -0,0 +1,21 @@
+/*
+Esteban Medero COP3503c Lab3 - Array Based Stack
+October 8, 2022
+This is part one of lab3 building a dynamically allocated array based stack
+a stack is a data structure that follows the First In Last Out principle (FILO)
+new items are stacked on top on the previous hence the first one in ends up on the 
+bottom.
+*/
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+template <typename ABStype>
+class ABS
+{
+    private:
+
+    public:
+
+};
