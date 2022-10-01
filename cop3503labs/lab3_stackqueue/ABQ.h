f346c2f12fe358d6c5923bc8cdae59d19d2f3bb0
Daniel-Eprog <39196999+Daniel-Eprog@users.noreply.github.com>
2022-10-01 13:58:31 -0400

    final commit lab3 35/35

--- cop3503labs/lab3_stackqueue/ABQ.h
+++ cop3503labs/lab3_stackqueue/ABQ.h
@@ -1,21 +1,216 @@
 /*
 Esteban Medero COP3503c Lab3 - Array Based Queue
 October 8, 2022
-This is part one of lab3 building a dynamically allocated array based queue.
+This is part two of lab3 building a dynamically allocated array based queue.
 a queue is a data structure that follows the First In First Out principle
 the next item is added behind the previous item hence the first item remains in 
 the front. Think people forming a line.
 */
 
+//////////////////*BEGIN PROGRAM*/////////////////////
+
+#ifndef ABQ_H
+#define ABQ_H
+
 #include <iostream>
 #include <string>
-#include <fstream>
 
-template <typename ABStype>
-class ABS
+using namespace std;
+
+//Declare template class 
+template <typename ABQType>
+class ABQ
 {
-    private:
+    //Declare scale factor global
+    float scaleFactor = 2.0f;
 
+    //Declare private variables
+    private:
+        ABQType *queueArray = nullptr;
+        int currentSize = 0;
+        int queueIndex = 0;
+        int maximumCapacity = 1;
+
+    //Declare public class methods
     public:
+        ABQ();
+        ABQ(int capacity);
+        ABQ(const ABQ &rhs);
+        ABQ& operator=(const ABQ &rhs);
+        ~ABQ();
+        void enqueue(ABQType data);
+        ABQType dequeue();
+        ABQType peek();
+        int getSize();
+        int getMaxCapacity();
+        ABQType* getData();
 
 };
+
+//default constructor
+template <typename ABQType>
+ABQ<ABQType>::ABQ()
+{
+    currentSize = 0;
+    maximumCapacity = 1;
+    queueArray = new ABQType[maximumCapacity];
+}
+
+//overloaded constructor
+template <typename ABQType>
+ABQ<ABQType>::ABQ(int capacity)
+{
+    currentSize = 0;
+    maximumCapacity = capacity;
+    queueArray = new ABQType[maximumCapacity];
+}
+        
+//copy constructor
+template <typename ABQType>
+ABQ<ABQType>::ABQ(const ABQ &rhs)
+{
+    this->currentSize = rhs.currentSize; //sets current size to right hand side
+    this->maximumCapacity = rhs.maximumCapacity; // sets max capacity to right hand side
+    this->queueArray = new ABQType[rhs.maximumCapacity]; //allocates space for a new queue with size of right hand side
+    //copys right hand side values into new queue one by one "deep copy"
+    for(int i = 0; i < maximumCapacity; ++i)
+    {
+        this->queueArray[i] = rhs.queueArray[i];
+    }
+}
+
+//overloaded copy operator
+template <typename ABQType>
+ABQ<ABQType>& ABQ<ABQType>::operator=(const ABQ &rhs)
+{
+    this->currentSize = rhs.currentSize; //sets current size to right hand side
+    this->maximumCapacity = rhs.maximumCapacity;// sets max capacity to right hand side
+    delete this->queueArray; //ensures queue to is clear for copy
+    this->queueArray = new ABQType[rhs.maximumCapacity]; //allocates memory on queue
+    //copys right hand side values into new queue one by one "deep copy"
+    for(int i = 0; i < maximumCapacity; ++i)
+    {
+        this->queueArray[i] = rhs.queueArray[i];
+    }
+    
+    //return new copied queue
+    return *this;
+}
+
+//destructor deallocates space when class object goes out of scope
+template <typename ABQType>
+ABQ<ABQType>::~ABQ()
+{
+    delete[] this->queueArray;
+}
+
+//push method adds new item to the stack
+template <typename ABQType>
+void ABQ<ABQType>::enqueue(ABQType data)
+{
+    //reallocates space if queue is at capacity
+    if(this->getSize() == this->getMaxCapacity())
+    {
+        //allocates new space for temp queue increased by scale factor
+        ABQType* tempQueue = new ABQType[this->getSize() * static_cast<int>(scaleFactor)];
+        //performs one by one deep copy
+        for(int i = 0; i < this->getSize(); ++i)
+        {
+            tempQueue[i] = this->queueArray[i];
+        }
+        //adds last value into new array
+        tempQueue[this->getSize()] = data;
+        //increases maximum capacity 
+        this->maximumCapacity *= scaleFactor;
+        //deletes memory allocated for the queue array
+        delete[] this->queueArray;
+        //assigns queueArray pointer to the temp queue
+        this->queueArray = tempQueue;
+        //increases current size by 1
+        this->currentSize++;
+    }
+    else
+    {
+        //adds new element and increases size by 1
+        this->queueArray[this->getSize()] = data;
+        this->currentSize++;
+    }
+
+}
+
+//removes element from the array and adjusts size if necessary
+template <typename ABQType>
+ABQType ABQ<ABQType>::dequeue()
+{
+    //throws runtime error if queue is empty
+    if(this->getSize() <= 0)
+    {
+        throw std::runtime_error("Queue is empty!");
+    }
+    //saves local variable with value of last in
+    ABQType frontValue = this->queueArray[this->queueIndex];
+    //removes size of array by one
+    this->currentSize -= 1;
+    this->queueIndex += 1;
+    //if size of array is less than half readjusts size
+    if(this->getSize() < (this->getMaxCapacity()/scaleFactor))
+    {
+        //allocate temp queue with memory size of current number of elements
+        ABQType* tempQueue = new ABQType[this->getSize()];
+        //performs deep copy
+        for(int i = 0; i < this->getSize(); ++i)
+        {
+            tempQueue[i] = this->queueArray[i + this->queueIndex];
+        }
+        //reset queue index
+        this->queueIndex = 0;
+        //readjusts maximum size by scale factor
+        this->maximumCapacity = this->maximumCapacity/scaleFactor;
+        //deallocates original array
+        delete[] this->queueArray;
+        //assigns original array to temp queue
+        this->queueArray = tempQueue;
+    } 
+    
+    //returns value thats been popped off
+    return frontValue;
+
+}
+
+//looks at top value without removing it
+template <typename ABQType>
+ABQType ABQ<ABQType>::peek()
+{
+    //throws runtime error if queue is empty
+    if(this->getSize() <= 0)
+    {
+        throw std::runtime_error("Queue is empty!");
+    }
+    //returns value of top value
+    return this->queueArray[this->queueIndex];
+}
+
+//returns current size of array
+template <typename ABQType>
+int ABQ<ABQType>::getSize()
+{
+    return this->currentSize;
+}
+
+//returns maximum capacity 
+template <typename ABQType>
+int ABQ<ABQType>::getMaxCapacity()
+{
+    return this->maximumCapacity;
+}
+
+//returns queue array
+template <typename ABQType>
+ABQType* ABQ<ABQType>::getData()
+{
+    return this->queueArray;
+}
+
+//////////////////*END PROGRAM*/////////////////////
+
+#endif
--- cop3503labs/lab3_stackqueue/ABS.h
+++ cop3503labs/lab3_stackqueue/ABS.h
@@ -3,19 +3,210 @@ Esteban Medero COP3503c Lab3 - Array Based Stack
 October 8, 2022
 This is part one of lab3 building a dynamically allocated array based stack
 a stack is a data structure that follows the First In Last Out principle (FILO)
-new items are stacked on top on the previous hence the first one in ends up on the 
-bottom.
+new items are stacked on top of the previous hence the first one in ends up on the 
+bottom. Items are then popped off from the top. 
 */
+
+//////////////////*BEGIN PROGRAM*/////////////////////
+
+#ifndef ABS_H
+#define ABS_H
 
 #include <iostream>
 #include <string>
-#include <fstream>
 
-template <typename ABStype>
+using namespace std;
+
+//Declare template class 
+template <typename ABSType>
 class ABS
 {
-    private:
+    //Declare scale factor global
+    float scaleFactor = 2.0f;
 
+    //Declare private variables
+    private:
+        ABSType *stackArray = nullptr;
+        int currentSize = 0;
+        int maximumCapacity = 1;
+
+    //Declare public class methods
     public:
+        ABS();
+        ABS(int capacity);
+        ABS(const ABS &rhs);
+        ABS& operator=(const ABS &rhs);
+        ~ABS();
+        void push(ABSType data);
+        ABSType pop();
+        ABSType peek();
+        int getSize();
+        int getMaxCapacity();
+        ABSType* getData();
 
 };
+
+//default constructor
+template <typename ABSType>
+ABS<ABSType>::ABS()
+{
+    currentSize = 0;
+    maximumCapacity = 1;
+    stackArray = new ABSType[maximumCapacity];
+}
+
+//overloaded constructor
+template <typename ABSType>
+ABS<ABSType>::ABS(int capacity)
+{
+    currentSize = 0;
+    maximumCapacity = capacity;
+    stackArray = new ABSType[maximumCapacity];
+}
+        
+//copy constructor
+template <typename ABSType>
+ABS<ABSType>::ABS(const ABS &rhs)
+{
+    this->currentSize = rhs.currentSize; //sets current size to right hand side
+    this->maximumCapacity = rhs.maximumCapacity; // sets max capacity to right hand side
+    this->stackArray = new ABSType[rhs.maximumCapacity]; //allocates space for a new stack with size of right hand side
+    //copys right hand side values into new stack one by one "deep copy"
+    for(int i = 0; i < maximumCapacity; ++i)
+    {
+        this->stackArray[i] = rhs.stackArray[i];
+    }
+}
+
+//overloaded copy operator
+template <typename ABSType>
+ABS<ABSType>& ABS<ABSType>::operator=(const ABS &rhs)
+{
+    this->currentSize = rhs.currentSize; //sets current size to right hand side
+    this->maximumCapacity = rhs.maximumCapacity;// sets max capacity to right hand side
+    delete this->stackArray; //ensures stack to is clear for copy
+    this->stackArray = new ABSType[rhs.maximumCapacity]; //allocates memory on stack
+    //copys right hand side values into new stack one by one "deep copy"
+    for(int i = 0; i < maximumCapacity; ++i)
+    {
+        this->stackArray[i] = rhs.stackArray[i];
+    }
+    
+    //return new copied stack
+    return *this;
+}
+
+//destructor deallocates space when class object goes out of scope
+template <typename ABSType>
+ABS<ABSType>::~ABS()
+{
+    delete[] this->stackArray;
+}
+
+//push method adds new item to the stack
+template <typename ABSType>
+void ABS<ABSType>::push(ABSType data)
+{
+    //reallocates space if stack is at capacity
+    if(this->getSize() == this->getMaxCapacity())
+    {
+        //allocates new space for temp stack increased by scale factor
+        ABSType* tempStack = new ABSType[this->getSize() * static_cast<int>(scaleFactor)];
+        //performs one by one deep copy
+        for(int i = 0; i < this->getSize(); ++i)
+        {
+            tempStack[i] = this->stackArray[i];
+        }
+        //adds last value into new array
+        tempStack[this->getSize()] = data;
+        //increases maximum capacity 
+        this->maximumCapacity *= scaleFactor;
+        //deletes memory allocated for the stack array
+        delete[] this->stackArray;
+        //assigns stackArray pointer to the temp stack
+        this->stackArray = tempStack;
+        //increases current size by 1
+        this->currentSize++;
+    }
+    else
+    {
+        //adds new element and increases size by 1
+        this->stackArray[this->getSize()] = data;
+        this->currentSize++;
+    }
+
+}
+
+//removes element from the array and adjusts size if necessary
+template <typename ABSType>
+ABSType ABS<ABSType>::pop()
+{
+    //throws runtime error if stack is empty
+    if(this->getSize() <= 0)
+    {
+        throw std::runtime_error("Stack is empty!");
+    }
+    //saves local variable with value of last in
+    ABSType topValue = this->stackArray[this->getSize() - 1];
+    //removes size of array by one
+    this->currentSize -= 1;
+    //if size of array is less than half readjusts size
+    if(this->getSize() < (this->getMaxCapacity()/scaleFactor))
+    {
+        //allocate temp stack with memory size of current number of elements
+        ABSType* tempStack = new ABSType[this->getSize()];
+        //performs deep copy
+        for(int i = 0; i < this->getSize(); ++i)
+        {
+            tempStack[i] = this->stackArray[i];
+        }
+        //readjusts maximum size by scale factor
+        this->maximumCapacity = this->maximumCapacity/scaleFactor;
+        //deallocates original array
+        delete[] this->stackArray;
+        //assigns original array to temp stack
+        this->stackArray = tempStack;
+    } 
+    
+    //returns value thats been popped off
+    return topValue;
+
+}
+
+//looks at top value without removing it
+template <typename ABSType>
+ABSType ABS<ABSType>::peek()
+{
+    //throws runtime error if stack is empty
+    if(this->getSize() <= 0)
+    {
+        throw std::runtime_error("Stack is empty!");
+    }
+    //returns value of top value
+    return this->stackArray[this->getSize() - 1];
+}
+
+//returns current size of array
+template <typename ABSType>
+int ABS<ABSType>::getSize()
+{
+    return this->currentSize;
+}
+
+//returns maximum capacity 
+template <typename ABSType>
+int ABS<ABSType>::getMaxCapacity()
+{
+    return this->maximumCapacity;
+}
+
+//returns stack array
+template <typename ABSType>
+ABSType* ABS<ABSType>::getData()
+{
+    return this->stackArray;
+}
+
+//////////////////*END PROGRAM*/////////////////////
+
+#endif
